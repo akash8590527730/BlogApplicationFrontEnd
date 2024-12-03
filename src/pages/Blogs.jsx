eff7fcc78a9cb2f6dba5ede378b2df72eab2af33
akash8590527730 <akashkurian2020@gmail.com>
2024-12-03 12:04:17 +0530

    update

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -14,30 +14,30 @@ function Blogs() {
   return (
     <div>
       <div className="container mx-auto my-12 p-4">
-        <h1 className="text-2xl font-bold mb-6">All Blogs</h1>
-        <div className="mb-8">
+        <h1 className="text-2xl font-bold mb-6 text-center">All Blogs</h1>
+        <div className="mb-8 text-center">
           <input
             type="text"
             placeholder="Search by category..."
-            className="w-full sm:w-1/3 px-4 py-2 border rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full sm:w-1/3 px-4 py-2 border rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 transition-all duration-300 transform hover:scale-105"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6">
           {filteredBlogs && filteredBlogs.length > 0 ? (
             filteredBlogs.map((blog, index) => (
               <Link
                 to={`/blog/${blog._id}`}
                 key={index}
-                className="relative rounded-lg overflow-hidden shadow-md transform hover:scale-105 transition-transform duration-300"
+                className="relative rounded-lg overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-300"
               >
                 <img
                   src={blog?.blogImage?.url}
                   alt={blog?.title}
-                  className="w-full h-48 object-cover"
+                  className="w-full h-48 object-cover rounded-t-lg"
                 />
-                <div className="absolute inset-0 bg-black opacity-30"></div>
+                <div className="absolute inset-0 bg-black opacity-30 transition-opacity duration-300 hover:opacity-50"></div>
                 <div className="absolute bottom-4 left-4 text-white">
                   <h2 className="text-lg font-semibold">{blog?.title}</h2>
                   <p className="text-sm">{blog?.category}</p>
